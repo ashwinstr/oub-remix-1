afff052e98736b688b7460ffcfc8b4a297dd1f5d
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-02-01 12:04:21 +0530

    Update waifu.py

--- userbot/modules/waifu.py
+++ userbot/modules/waifu.py
@@ -148,25 +148,6 @@ async def _(hazmat):
     await hazmat.delete()
     return os.remove(downloaded_file_name)
 
-@register(outgoing=True, pattern=r"^.pat(?: |$)")
-async def pat(e):
-    global _pats
-
-    url = "https://headp.at/js/pats.json"
-    if not _pats:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url) as raw_resp:
-                resp = await raw_resp.text()
-        _pats = json.loads(resp)
-    pats = _pats
-
-    pats = [i for i in pats if os.path.splitext(i)[1] == ".gif"]
-
-    pat = random.choice(pats)
-    link = f"https://headp.at/pats/{urlencode(pat)}"
-
-    await asyncio.wait([e.respond(file=link, reply_to=e.reply_to_msg_id), e.delete()])
-
 
 CMD_HELP.update({
     "waifu":
